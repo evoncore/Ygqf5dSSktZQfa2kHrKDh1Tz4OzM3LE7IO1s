95a1bf2ca9305688a10ac960302a60ead79d2c75
Sentyurin V <mr.sentyurin@gmail.com>
2016-04-07 23:13:52 +0300

    Finish first version app

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
   def home
     if signed_in?
-      @micropost = current_user.microposts.build if signed_in?
+      @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
@@ -14,6 +14,10 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  end
+
+  def admin
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,32 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  match '/help', to: 'static_pages#help',       via: 'get'
+  match '/help',     to: 'static_pages#help',    via: 'get'
 
-  match '/about', to: 'static_pages#about',     via: 'get'
+  match '/about',    to: 'static_pages#about',   via: 'get'
 
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/contact',  to: 'static_pages#contact', via: 'get'
+
+  match '/admin',    to: 'static_pages#admin',   via: 'get'
 
   # Users
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
-  match '/sign_up', to: 'users#new',            via: 'get'
+  match '/sign_up',  to: 'users#new',            via: 'get'
+
+  resources :relationships, only: [:create, :destroy]
 
   # Sessions
 
   resources :sessions, only: [:new, :create, :destroy]
 
-  match '/sign_in', to: 'sessions#new',         via: 'get'
-  match '/sign_out', to: 'sessions#destroy',    via: 'delete'
+  match '/sign_in',  to: 'sessions#new',         via: 'get'
+  match '/sign_out', to: 'sessions#destroy',     via: 'delete'
 
   # Microposts
 
